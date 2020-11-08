cb6fa41ecc5e0fa0690e53250e4aa7e6da4c79f9
Liarr <liarr@foxmail.com>
2020-11-09 00:18:41 +0800

    完成 PermissionX 库的开发与测试

---
+++ Library/src/main/java/com/permissionx/liarrdev/InvisibleFragment.kt
@@ -0,0 +1,33 @@
+package com.permissionx.liarrdev
+
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
+class InvisibleFragment : Fragment() {
+
+    private var callback: PermissionCallback? = null
+
+    fun requestNow(callback: PermissionCallback, vararg permissions: String) {
+        this.callback = callback
+        requestPermissions(permissions, 1)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            val deniedList = ArrayList<String>()
+            for ((index, result) in grantResults.withIndex()) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    deniedList.add(permissions[index])
+                }
+            }
+            val allGranted = deniedList.isEmpty()
+            callback?.let { it(allGranted, deniedList) }
+        }
+    }
+}
+
+typealias PermissionCallback = (Boolean, List<String>) -> Unit
